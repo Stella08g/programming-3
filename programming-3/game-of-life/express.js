fa72089c6b9ba277cb65c81fc920f898442e281c
Stella08g <97609519+Stella08g@users.noreply.github.com>
2022-01-27 18:53:14 +0400

    Delete programming-3/game-of-life directory
    delate

--- programming-3/game-of-life/express.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.get("/name/:name", function(req, res){
-    var name = req.params.name;
-    res.send("<h1>Hello " + name + "</h1>");
-    });
